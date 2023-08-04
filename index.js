1b8c93f395dae6e79af4cf2442bf6c677bc52f5e
Sean La Tourette <sean.latourette@straightfire.tech>
2023-08-03 20:33:13 -0700

    allow all cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer();
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:8000",
+        origin: "*",
         methods: ['GET', "POST"],
     }
 });
