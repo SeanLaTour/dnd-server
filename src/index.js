5948e3fbfaf98202996fb185efe0656a3260cbbd
Sean La Tourette <sean.latourette@straightfire.tech>
2023-08-18 00:40:11 -0700

    api integration

--- src/index.js
+++ src/index.js
@@ -3,11 +3,48 @@ const http = require("http")
 const app = express();
 const { Server, Socket } = require("socket.io")
 const cors = require("cors");
-const { listenerCount } = require("process");
+const fs = require('fs');
+const path = require('path');
+const dbFilePath = path.join(__dirname, 'database.json');
 
 app.use(cors());
+app.use(express.json()); // For JSON
+app.use(express.text()); // For plain text
 
-const server = http.createServer();
+app.get("/map", (req, res) => {
+    const data = readDatabase();
+    res.send(data)
+})
+
+app.post("/map", (req, res) => {
+    let data = readDatabase();
+    const map = req.body
+    data.map = map
+    writeDatabase(data);
+    res.send(200)
+})
+
+app.get("/characters", (req, res) => {
+    const data = readDatabase();
+    res.send(data)
+})
+
+app.post("/characters", (req, res) => {
+    let data = readDatabase();
+    const characters = req.body
+
+    if(typeof data !== "object") {
+        data = {characters: characters}
+    }
+    else {
+        data.characters = characters
+    }
+
+    writeDatabase(data);
+    res.send(200)
+})
+
+const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
@@ -19,12 +56,24 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
     console.log("User Connected" + socket.id)
     socket.on("send_message", (data) => {
-        console.log(data)
+        console.log("connected")
         socket.broadcast.emit("recieve_message", data)
     })
 })
 
-
 server.listen(process.env.PORT || 3000, () => {
     console.log("SERVER RUNNING 3000")
 })
+
+function readDatabase() {
+  try {
+    const data = fs.readFileSync(dbFilePath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+}
+  
+function writeDatabase(data) {
+  fs.writeFileSync(dbFilePath, JSON.stringify(data, null, 2), 'utf8');
+}
